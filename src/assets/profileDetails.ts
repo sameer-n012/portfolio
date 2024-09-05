632bb38b56e2c20711d852cfcf7069369dd7daa4
sameer-n012 <sameer.narendran@gmail.com>
2024-09-04 19:23:48 -0500

    updated resume and profile

--- src/assets/profileDetails.ts
+++ src/assets/profileDetails.ts
@@ -1,31 +1,32 @@
 const profileDetails = {
-    email: 'sameer.narendran@gmail.com',
-    profileImgName: 'portrait.jpeg',
-    linkedinURL: 'https://linkedin.com/in/sameer-narendran',
-    githubURL: 'https://github.com/sameer-n012',
-    resumeURL: './assets/resume.pdf',
-    driveResumeURL: 'https://drive.google.com/file/d/1DAvo7yurIF6XlGv5TtwE01F_2SnpUQnF/view',
-    projectSourceURL: 'https://github.com/sameer-n012/portfolio-v2',
-    aboutMe:
-        "I'm Sameer Narendran, a second-year student at the University of \
+  email: "sameer.narendran@gmail.com",
+  profileImgName: "portrait.jpeg",
+  linkedinURL: "https://linkedin.com/in/sameer-narendran",
+  githubURL: "https://github.com/sameer-n012",
+  resumeURL: "./assets/resume.pdf",
+  driveResumeURL:
+    "https://drive.google.com/file/d/1DAvo7yurIF6XlGv5TtwE01F_2SnpUQnF/view",
+  projectSourceURL: "https://github.com/sameer-n012/portfolio",
+  aboutMe:
+    "I'm Sameer Narendran, a senior honors student at the University of \
         Wisconsin Madison studying Computer Science and Math. My primary \
-        interest is in AI and neural networks, but I also enjoy working on \
-        software projects in Java as well as full-stack applications. When I'm \
-        not in class or on my computer, I can usually be found reading science \
-        fiction novels or playing tennis outside with my friends.",
-    skills: [
-        'Java',
-        'C',
-        'Python',
-        'Javascript',
-        'SQL',
-        'MongoDB',
-        'Tensorflow & PyTorch',
-        'ReactJS & Redux',
-    ],
-    attribution: {
-        fa: 'https://fontawesome.com/',
-    }
+        interest is in Machine Learning and Large Language Models, but I also enjoy working on \
+        other projects in Java, C, and Rust. When I'm \
+        not in class, I enjoy reading science \
+        fiction novels and playing tennis outside with my friends.",
+  skills: [
+    "Java",
+    "C",
+    "Rust",
+    "Python",
+    "Javascript",
+    "SQL",
+    "PyTorch & LangChain",
+    "ReactJS & Redux",
+  ],
+  attribution: {
+    fa: "https://fontawesome.com/",
+  },
 };
 
 export default profileDetails;
